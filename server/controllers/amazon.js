59d84faf83cd299442fc1aac83a8c42ab9d2819a
Grayson Gilmore <gilmoreg@live.com>
2017-08-08 17:23:49 -0600

    Amazon price controller

--- server/controllers/amazon.js
+++ server/controllers/amazon.js
@@ -1,3 +1,22 @@
-exports.fetchPrices = (id) => {
-  return id;
+const amazon = require('amazon-affiliate-api');
+
+exports.fetchData = async (id) => {
+  const client = amazon.createClient({
+    awsId: process.env.AMAZON_ACCESS_KEY,
+    awsSecret: process.env.AMAZON_SECRET,
+    awsTag: process.env.AMAZON_ASSOCIATE_ID,
+  });
+  return client.itemSearch({
+    searchIndex: 'Toys',
+    keywords: `Lego ${id}`,
+    title: `${id}`,
+    responseGroup: 'OfferSummary,Small',
+  })
+    .then(results => results.Items.Item[0])
+    .then(item => ({
+      url: item.DetailPageURL,
+      title: item.ItemAttributes.Title,
+      price: item.OfferSummary.LowestNewPrice.Amount,
+    }))
+    .catch(err => console.error(err));
 };
--- server/controllers/price.js
+++ server/controllers/price.js
@@ -1,6 +1,6 @@
 const amazon = require('./amazon');
 
 exports.fetchPrices = async (req, res) => {
-  const amazonPrices = await amazon.fetchPrices(req.params.id);
-  res.status(200).json({ prices: amazonPrices });
+  const amazonPrices = await amazon.fetchData(req.params.id);
+  res.status(200).json({ amazon: amazonPrices });
 };
