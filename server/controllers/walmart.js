00b8e603b9506005439960634f5f8f3a87606be1
Grayson Gilmore <gilmoreg@live.com>
2017-08-08 19:45:36 -0600

    Remove debuggers

--- server/controllers/walmart.js
+++ server/controllers/walmart.js
@@ -1,7 +1,6 @@
 global.fetch = require('node-fetch');
 
 exports.fetchData = (id) => {
-  console.log('walmart', id);
   const url = `http://api.walmartlabs.com/v1/search?query=Lego+${id}&format=json&categoryId=4171_4186_1044000&apiKey=${process.env.WALMART_KEY}`;
   return global.fetch(url)
     .then(res => res.json())
